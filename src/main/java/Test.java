3f8b7114dcd7f48b085ac26d1d53133ee50f4a29
Donne <17685811513@163.com>
2020-11-10 17:31:50 +0800

    ADD:TestMaster

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * 2 * @Author: donne
+ * 3 * @Date: 2020/11/10 17:30
+ * 4
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("master ===== 1");
+    }
+}
